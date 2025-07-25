9aa68ecf6ebbdd9438c4706514d83666559aa4d6
alishbye <alishba903.sie@gmail.com>
2025-07-25 14:52:46 -0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Coffee-shop/'
+  plugins: [react(),tailwindcss()],
+  base:'/Coffee-shop/'
 })

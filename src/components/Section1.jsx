dd14e6a27840b3dca753967d52458b1807300c19
alishbye <alishba903.sie@gmail.com>
2025-07-25 15:09:36 -0700

    ghada

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -14,6 +14,7 @@ const Section1 = () => {
     offset: ["start start", "end end"]
   });
 
+    const ghada = 5;
   
   // Cup animations
  const scaleCup = useTransform(scrollYProgress, [0, 0.20], [1.4, 4.8]);
